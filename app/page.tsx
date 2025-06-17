78e0171a065c6bb615cb7ec6ba18363b6dd2e425
AymanSdk <ayman.sdk123@outlook.com>
2025-06-17 19:25:30 +0100

    Add new components and update dependencies; modify global styles and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div className='flex flex-col items-center justify-center gap-2'>
+      <h1 className='text-2xl underline'>Welcome to my Saas App</h1>
+      <Link href='/sign-in'>
+        <Button variant='default'>Get started</Button>
+      </Link>
+      <Link href='/'>
+        <Button variant='default'>Go Home</Button>
+      </Link>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
